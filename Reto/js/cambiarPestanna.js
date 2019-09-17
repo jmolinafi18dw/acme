4002f607646c957a728419edeb84fbbdef03d20d
David <44891385+Belins@users.noreply.github.com>
2019-09-17 08:08:16 +0200

    Subo mi parte de la página web

---
+++ Reto/js/cambiarPestanna.js
@@ -0,0 +1,26 @@
+function cambiarPestanna(pestannas,pestanna) {
+    
+    pestanna = document.getElementById(pestanna.id);
+    listaPestannas = document.getElementById(pestannas.id);
+    
+    cpestanna = document.getElementById('c'+pestanna.id);
+    listacPestannas = document.getElementById('contenido'+pestannas.id);
+    
+    i=0;
+    // Recorre la lista ocultando todas las pestañas y restaurando el fondo y el padding de las pestañas.
+    while (typeof listacPestannas.getElementsByTagName('div')[i] != 'undefined'){
+        $(document).ready(function(){
+            $(listacPestannas.getElementsByTagName('div')[i]).css('display','none');
+            $(listaPestannas.getElementsByTagName('li')[i]).css('background','');
+            $(listaPestannas.getElementsByTagName('li')[i]).css('padding-bottom','');
+        });
+        i += 1;
+    }
+
+    $(document).ready(function(){
+        $(cpestanna).css('display','');
+        $(pestanna).css('background','dimgray');
+        $(pestanna).css('padding-bottom','2px');
+    });
+
+}
